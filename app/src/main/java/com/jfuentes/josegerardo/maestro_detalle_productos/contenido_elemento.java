b700ce5ac4ad2c9370bbe7061dadbc6b357e47bb
Jhovany <vfjhovanyitsolutions@gmail.com>
2019-10-14 14:41:08 -0600

    avanze4

---
+++ app/src/main/java/com/jfuentes/josegerardo/maestro_detalle_productos/contenido_elemento.java
@@ -0,0 +1,16 @@
+package com.jfuentes.josegerardo.maestro_detalle_productos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.jfuentes.josegerardo.R;
+
+public class contenido_elemento extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contenido_elemento);
+    }
+}
---
+++ app/src/main/java/com/jfuentes/josegerardo/maestro_detalle_productos/lista_productos.java
@@ -0,0 +1,16 @@
+package com.jfuentes.josegerardo.maestro_detalle_productos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.jfuentes.josegerardo.R;
+
+public class lista_productos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_productos);
+    }
+}
