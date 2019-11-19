93c5edde9c4fd45eeee0044ec8a2cd39654b66aa
Jhovany <vfjhovanyitsolutions@gmail.com>
2019-11-18 19:23:42 -0600

    avanze 12

--- app/src/main/java/com/jfuentes/josegerardo/maestro_detalle_productos/adaptador_maestro/nuevo_producto.java
+++ app/src/main/java/com/jfuentes/josegerardo/maestro_detalle_productos/adaptador_maestro/nuevo_producto.java
@@ -31,34 +31,8 @@ public class nuevo_producto extends AppCompatActivity {
         setTitle("Nuevo producto");
 
         lista_sucursales=findViewById(R.id.lista_sucu);
-        lista_categorias=findViewById(R.id.lista_cate);
+        lista_categorias=findViewById(R.id.lista_cate       );
 
-        conexiones_base con=new conexiones_base(this);
-
-        String[] va={"",""};
-        String[] va2={"","",""};
-        String url="";
-        String url2="";
-        try {
-            url=conexiones_base.sentencia(new sucursales(), con.getIpe(), con.getPuertoe(), utilidades.REGISTROS, va);
-            lista_su=con.extraer(url, utilidades.SUCURSALES);
-            lista_su.add(new sucursales("0","Elija la sucursal"));
-            ArrayAdapter<entity> adapta=new ArrayAdapter<entity>(this, android.R.layout.simple_spinner_item,lista_su);
-            lista_sucursales.setAdapter(adapta);
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        }
-
-        try {
-            url2=conexiones_base.sentencia(new categorias(), con.getIpe(), con.getPuertoe(), utilidades.REGISTROS, va2);
-            lista_ca=con.extraer(url2, utilidades.CATEGORIAS);
-            lista_ca.add(new categorias("-", "Elija la categoria","0"));
-            ArrayAdapter<entity> adapta2=new ArrayAdapter<entity>(this, android.R.layout.simple_spinner_item,lista_ca);
-            lista_categorias.setAdapter(adapta2);
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        }
     }
-
 
 }
